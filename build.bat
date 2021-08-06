f396a4a4da95d5e610acac30ff2aba9d738bb6d0
kien-trung <kientrung.khuong@officience.com>
2021-08-06 08:53:17 +0700

    implement build script

---
+++ build.bat
@@ -0,0 +1,29 @@
+@echo off
+
+echo **************************************************************************************
+echo                                        BUILD
+echo **************************************************************************************
+msbuild StateMachine.sln /P:Configuration=Release
+set BUILD_STATUS=%ERRORLEVEL% 
+if %BUILD_STATUS%==0 goto TEST 
+if not %BUILD_STATUS%==0 goto FAIL
+
+:TEST
+echo **************************************************************************************
+echo                                        TEST
+echo **************************************************************************************
+dotnet vstest Tests\bin\Release\netcoreapp2.1\StateMachine.Tests.dll
+set TEST_STATUS=%ERRORLEVEL% 
+if %TEST_STATUS%==0 goto FINISH 
+if not %TEST_STATUS%==0 goto FAIL
+goto FINISH
+
+:FINISH
+xcopy /Y StateMachine\bin\Release\*.nupkg Out\
+echo Publish to BaGet: nuget push -Source https://nuget.steelseries.io/v3/index.json package.nupkg
+pause 
+exit /b 1
+
+:FAIL
+pause 
+exit /b 1 
